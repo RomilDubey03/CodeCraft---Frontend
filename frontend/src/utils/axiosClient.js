7e0131c6744bb0805cc4fc8faf9834a5ba81b3cd
Romil Dubey <romildubey03@gmail.com>
2025-12-16 23:19:56 +0530

    update backend url

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "https://code-craft-frontend-hwrf.vercel.app/",
+    baseURL: "https://code-craft-frontend-hwrf.vercel.app/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
