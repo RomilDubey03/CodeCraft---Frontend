0b643766eadcc5146bb0ec8a8d4fb7e05c97ef3c
Romil Dubey <romildubey03@gmail.com>
2025-12-17 00:39:22 +0530

    deploying attempt changes

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "https://code-craft-frontend-hwrf.vercel.app/api/v1",
+    baseURL:
+        import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
